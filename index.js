ebaa80f2ca2e1177278ac906621416f28eeb0368
tomasbudrikas10 <82083897+tomasbudrikas10@users.noreply.github.com>
2024-03-25 18:52:17 +0200

    Add Sequelize

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const { Sequelize } = require('sequelize')
+const sequelize = new Sequelize('restoranas', 'root', '', {
+    host: "localhost",
+    dialect: 'mysql'
+})
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`Example app listening on port ${port}`)
+    try {
+        await sequelize.authenticate()
+    } catch(e) {
+        console.error(e)
+    }
 })
