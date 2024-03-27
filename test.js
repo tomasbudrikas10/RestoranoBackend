d9672725f6382d4a574e2c932bd9d0badd44c5dc
tomasbudrikas10 <82083897+tomasbudrikas10@users.noreply.github.com>
2024-03-27 09:42:22 +0200

    Add GET Product By ID Tests

--- test.js
+++ test.js
@@ -13,3 +13,33 @@ describe("Get All Products", () => {
             })
     });
 });
+
+describe("Get Product By ID", () => {
+    test("Get Product With Existing ID", () => {
+        return request(app)
+            .get("/products/3")
+            .then(response => {
+                expect(response.headers["content-type"]).toMatch(/json/);
+                expect(response.statusCode).toBe(200);
+                expect(response.body.data.name).toBe("Produktas C");
+            })
+    })
+    test("Get Product With Non-Existing ID", () => {
+        return request(app)
+            .get("/products/4")
+            .then(response => {
+                expect(response.headers["content-type"]).toMatch(/json/);
+                expect(response.statusCode).toBe(404);
+                expect(response.body.errors.length).toBeGreaterThan(0)
+            })
+    })
+    test("Get Product With Invalid ID", () => {
+        return request(app)
+            .get("/products/a")
+            .then(response => {
+                expect(response.headers["content-type"]).toMatch(/json/);
+                expect(response.statusCode).toBe(400);
+                expect(response.body.errors.length).toBeGreaterThan(0)
+            })
+    })
+})
